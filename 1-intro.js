d2d4787b6be580101967304e712f3bdff5392c99
Eric Zhou <ericzhou@Erics-MacBook-Pro.local>
2023-02-16 20:48:32 -0500

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,11 @@
+console.log('Hello');
+
+const amount = 9;
+
+if (amount < 10){
+    console.log("small")
+}
+
+else {
+    console.log("big")
+}
---
+++ 2-globals.js
@@ -0,0 +1,11 @@
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules (CommonJS)
+// module - info about current modeule (file)
+// process - info about env where the program is being executed
+
+console.log(__dirname)
+
+setInterval(() =>{
+    console.log('hi')
+}, 1000)
